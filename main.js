3bd154aae08861036997577a02c53ee22df5ba09
jeongah <55934468+jeongah11@users.noreply.github.com>
2021-11-19 20:43:44 +0900

    JS: work filtering

--- main.js
+++ main.js
@@ -22,18 +22,41 @@ navbarMenu.addEventListener('click', (event) => {
   scrollIntoView(link);
 });
 
-//#home 'Contact Me' button
+//Home 'Contact Me' button
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
-//#home 콘텐츠 scroll 시 투명 효과
+//Home 콘텐츠 scroll 시 투명 효과
 const home = document.querySelector('#home');
 const homeHeight = home.getBoundingClientRect().height;
 const homeContent = document.querySelector('.home__container');
 document.addEventListener('scroll', () => {
   homeContent.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Works filtering
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projets = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter ==null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out'); //filtering 시작 project 사라지는 ani
+  setTimeout(() => {
+    //클릭한 버튼과 일치하는 data-type filter
+    projets.forEach((project) => {
+      if(filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');//filtering 후 project 나타남
+  }, 300);
 });
 
 //toppage 이동 button
