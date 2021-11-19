1647c213cfe0b977cdf9207c87b2ee717e4dec23
jeongah <55934468+jeongah11@users.noreply.github.com>
2021-11-19 15:35:51 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -36,6 +36,19 @@ document.addEventListener('scroll', () => {
   homeContent.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+//toppage 이동 button
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight/2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
